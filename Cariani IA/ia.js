16b666e61427447b81b16a69c64522f6112fd488
luizkamaroski <111889547+luizkamaroski@users.noreply.github.com>
2024-07-01 11:03:34 -0300

    Add files via upload

---
+++ Cariani IA/ia.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixasAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultado = document.querySelector(".caixa-Resultados");
+const textoResultado = document.querySelector(".Texto-Resultado")
+const caixaresultado1 = document.querySelector(".caixa-Principal")
+
+const Perguntas = [
+
+    {
+      enunciado:"É preciso ter uma boa alimentação?",alternativas: [
+        "sim"
+      ]  
+    }
+
+]
